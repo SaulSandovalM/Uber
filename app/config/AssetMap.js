7b91edf6f69938254c6d664f2ebdc294481b3502
SaulSandovalM <sauldevelop@gmail.com>
2017-07-07 21:18:41 -0500

    quitando archivos

--- app/config/AssetMap.js
+++
@@ -1,6 +0,0 @@
-export default {
-  hamburger: require('../images/icon-hamburger.png'),
-  home: require('../images/icon-home.png'),
-  recent: require('../images/icon-recent.png'),
-  'arrow-left': require('../images/icon-arrow-left.png'),
-}
